16ed2c0fe429c9c707983c73c9e6f9d0199caf21
rm49024 <rm49024@WL446743>
2017-08-21 16:09:49 +0100

    Palindrome added. Bowling in progress.

---
+++ Bowling/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Bowling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the scores separated by spaces.");
+            var scores = Console.ReadLine();
+            int currentScore = 0;
+            foreach (string frame in scores.Split(' '))
+            {
+                int framescore = CalculateScore(frame);
+                currentScore += framescore;
+            }
+
+            Console.Write($"Final Score: {currentScore}");
+            Console.ReadKey();
+
+        }
+
+        private static int CalculateScore(string frame)
+        {
+            int framescore = 0;
+            foreach (char turn in frame.ToUpper())
+            {
+                if (turn == 'X')
+                {
+                    framescore += 10;
+                }
+
+                else if (turn == '/')
+                {
+                    framescore = 10;
+                }
+
+                else if (turn == '-')
+                {
+                    // do nothing
+                }
+
+                else
+                {
+                    framescore += int.Parse(turn.ToString());
+                }
+            }
+
+            return framescore;
+        }
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(Console.ReadLine()));
+            Console.ReadKey();
+        }
+
+        public static bool IsPalindrome(string word)
+        {
+            word = word.ToLower();
+            var len = word.Length;
+            if (len < 2) return true;
+
+            if (word[0] != word[len - 1]) return false;
+
+            return IsPalindrome(word.Substring(1, len - 2));
+        }
+    }
+}

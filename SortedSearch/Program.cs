e8e705775b1d436c43984c37a20907ec798d95f7
Ravi <Ravi@RAV>
2017-09-01 00:37:57 +0100

    more katas

---
+++ SortedSearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SortedSearch
+{
+    public class SortedSearch
+    {
+        public static int CountNumbers(int[] sortedArray, int lessThan)
+        {
+            if (sortedArray[0] >= lessThan) return 0;
+            if (sortedArray[sortedArray.Length - 1] < lessThan) return sortedArray.Length;
+            return Count(sortedArray, lessThan);
+        }
+
+        private static int Count(int[] sortedArray, int lessThan)
+        {
+            int first = 1, last = sortedArray.Length - 1;
+            while (first <= last)
+            {
+                int mid = (first + last) / 2;
+                if (lessThan < sortedArray[mid])
+                    last = mid - 1;
+                if (lessThan > sortedArray[mid])
+                    first = mid + 1;
+                else
+                    return mid;
+            }
+
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CountNumbers(new int[] { 1, 3, 5, 7 }, 3));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TwoSum
+{
+    using System;
+    using System.Collections.Generic;
+
+    class TwoSum
+    {
+        public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                int num = list[i];
+                int remain = sum - num;
+                int j;
+                if (dict.TryGetValue(remain, out j))
+                    return new Tuple<int, int>(i, j);
+
+                if (!dict.ContainsKey(num)) dict.Add(num, i);
+            }
+
+            return null;
+        }
+
+        public static void Main(string[] args)
+        {
+            Tuple<int, int> indices = FindTwoSum(new List<int>() { 1, 3, 5, 7, 9 }, 12);
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+        }
+    }
+}

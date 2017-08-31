d0520b25015715a0a475fbaf0dfe2be03a6f1ae3
rm49024 <rm49024@WL446743>
2017-08-31 17:11:36 +0100

    more katas

---
+++ FolderNames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace FolderNames
+{
+    class Folders
+    {
+        public static IEnumerable<string> FolderNames(string xml, char startingLetter)
+        {
+            var xDoc = XDocument.Parse(xml);
+            var a = xDoc.Descendants("folder").Where(x => x.Attribute("name").Value.StartsWith(startingLetter.ToString())).Select(x => x.Attribute("name").Value).ToList();
+            return a;
+        }
+
+        public static void Main(string[] args)
+        {
+            string xml =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<folder name=\"c\">" +
+                    "<folder name=\"program files\">" +
+                        "<folder name=\"uninstall information\" />" +
+                    "</folder>" +
+                    "<folder name=\"users\" />" +
+                "</folder>";
+
+            foreach (string name in FolderNames(xml, 'u'))
+                Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FolderPath/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FolderPath
+{
+    class Path
+    {
+        public string CurrentPath { get; private set; }
+
+        public Path(string path)
+        {
+            this.CurrentPath = path;
+        }
+
+        public void Cd(string newPath)
+        {
+            var arr = newPath.Split('/');
+            foreach (var item in arr)
+            {
+                switch (item)
+                {
+                    case "..":
+                        this.CurrentPath = this.CurrentPath.Substring(0, Array.LastIndexOf(CurrentPath.ToCharArray(), "/"));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Path path = new Path("/a/b/c/d");
+            path.Cd("../x");
+            Console.WriteLine(path.CurrentPath);
+        }
+    }
+}
